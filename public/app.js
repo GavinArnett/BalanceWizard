41974ffda57d898f4f4db47f2628b0a0f2dd8c08
Gavin <gmanarnett@gmail.com>
2024-02-05 18:44:57 -0500

    First commit

---
+++ public/app.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbG4Al-EoL2yRkPoyjylVcX6y5uqGuWY0",
+  authDomain: "balance-wizard.firebaseapp.com",
+  projectId: "balance-wizard",
+  storageBucket: "balance-wizard.appspot.com",
+  messagingSenderId: "787437428043",
+  appId: "1:787437428043:web:8115500609a47d8cc2dcce",
+  measurementId: "G-KFPB7N0909"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
